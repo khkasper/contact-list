9ad05beda4ae75a176bbbc1592c9046676ed9a83
kristiano <kristianokasper@gmail.com>
2022-07-31 18:31:01 -0300

    Initial setup

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World</h1>
+        <button className="btn btn-primary"> Hello Bootstrap</button>
+    </div>
+  );
+}
+
+export default App;
