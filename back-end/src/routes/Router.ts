75b8d5eb959c1d3d569c192291c69ea488ce48c9
kristiano <kristianokasper@gmail.com>
2022-08-01 17:38:08 -0300

    feat: add connection, app setup and routes for server

---
+++ back-end/src/routes/Router.ts
@@ -0,0 +1,22 @@
+import {Router} from 'express';
+import Controller from '../app/controllers';
+
+class CustomRouter<T> {
+    public router: Router;
+
+    constructor() {
+        this.router = Router();
+    }
+
+    public addRoute(
+        controller: Controller<T>,
+        route: string = controller.route,
+    ) {
+        this.router.get(route, controller.read);
+        this.router.post(route, controller.create);
+        this.router.put(`${route}/:id`, controller.update);
+        this.router.delete(`${route}/:id`, controller.delete);
+    }
+}
+
+export default CustomRouter;
